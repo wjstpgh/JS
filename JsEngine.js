d41fce806c60c69874b69dab6e43d637f9c0d8b1
wjstpgh <sehoJ3756@gmail.com>
2022-10-30 13:46:10 +0900

    v8엔진 구동원리, parse, jit컴파일러

---
+++ JsEngine.js
@@ -0,0 +1,15 @@
+//parser, AST
+function parser(code){
+  return code.split(/\s+/)
+}
+
+parser('var a = 5')
+
+//interpreter & compiler, JIT just in time compiler
+function add(x,y){
+  return x+y;
+}
+
+for (let i=0; i<10; i++){
+  console.log(add(6,4));
+}
