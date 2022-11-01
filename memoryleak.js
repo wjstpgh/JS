35366e2a4d72160cb428503ca3ba7914b55c7334
wjstpgh <sehoJ3756@gmail.com>
2022-11-01 15:47:59 +0900

    js 런타임, 메모리 누수

---
+++ memoryleak.js
@@ -0,0 +1,20 @@
+//memoryleak
+let arr = [];
+for (let i = 5; i > 1; i++) {
+  arr.push(i - 1)
+}
+
+//global variable
+var a = 1;
+var b = 1;
+var c = 1;
+
+//event listeners
+var element = document.getElementById('button');
+element.addEventListener('click', onClick);
+
+//setInterval
+setInterval(() => {
+  //객체 참조
+
+})
---
+++ runtime.js
@@ -0,0 +1,5 @@
+console.log('1');
+setTimeout(() => { console.log('2') }, 1000);
+console.log('3');
+
+//http://latentflip.com/loupe/?code=ZnVuY3Rpb24gcHJpbnRIZWxsbygpIHsNCiAgICBjb25zb2xlLmxvZygnSGVsbG8gZnJvbSBiYXonKTsNCn0NCg0KZnVuY3Rpb24gYmF6KCkgew0KICAgIHNldFRpbWVvdXQocHJpbnRIZWxsbywgMzAwMCk7DQp9DQoNCmZ1bmN0aW9uIGJhcigpIHsNCiAgICBiYXooKTsNCn0NCg0KZnVuY3Rpb24gZm9vKCkgew0KICAgIGJhcigpOw0KfQ0KDQpmb28oKTs%3D!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
