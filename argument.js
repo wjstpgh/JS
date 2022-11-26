df257b28f33b54f79758220c307f3821b772f9e9
wjstpgh <sehoJ3756@gmail.com>
2022-11-26 11:29:01 +0900

    arguments 코드 추가

--- argument.js
+++ argument.js
@@ -1,11 +1,11 @@
 //function expression
 var canada = function () {
-  console.log('cold')
-}
+  console.log("cold");
+};
 
 //function declaration
 function india() {
-  console.log('warm')
+  console.log("warm");
 }
 
 //function invocation/call/execution
@@ -15,15 +15,29 @@ india();
 function meet(person1, person2) {
   console.log(arguments);
   console.log(Array.from(arguments));
-  return `${person1} meet ${person2}`
+  return `${person1} meet ${person2}`;
 }
 
-meet('Kim', 'Park');
+meet("Kim", "Park");
 
 function meet2(...args) {
   console.log(args);
   console.log(Array.from(arguments));
-  return `${args[0]} meet ${args[1]}`
+  return `${args[0]} meet ${args[1]}`;
 }
 
-meet2('Kim', 'Park');
+meet2("Kim", "Park");
+
+const meet3 = (person1, person2) => {
+  console.log(arguments);
+  return `${Array.from(arguments)[2]} meet ${Array.from(arguments)[3]}`;
+};
+
+meet3("Kim", "Park", "1", "2");
+
+const meet4 = (person1, person2, ...args) => {
+  console.log(args);
+  return `${args[0]} meet ${args[1]}`;
+};
+
+meet4("Kim", "Park", "1", "2");
