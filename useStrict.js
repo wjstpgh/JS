bbdf85a4f787bf88686e42fd172838a8f6601af9
wjstpgh <sehoJ3756@gmail.com>
2022-12-04 18:06:00 +0900

    usestrict등 js의 문법불확실성

--- useStrict.js
+++ useStrict.js
@@ -1,15 +1,14 @@
-'use strict'
+// 'use strict'
 function weird() {
-  height = 50;//when strict isnt work
+  height = 50;//when strict, isnt work
   return height
 }
 
 console.log(weird());
 
 var hey = function hi() {
-
   return 'hey'
 }
 
-hey();
-// hi();
+// hey();
+hi();//throw Err
