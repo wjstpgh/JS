97a2e529e6fc6605ad72227dfb3bb74a221f563b
wjstpgh <sehoJ3756@gmail.com>
2022-12-04 18:59:32 +0900

    실행컨텍스트의 스코프체인의 변수 참조방식

--- scopeChain.js
+++ scopeChain.js
@@ -1,34 +1,37 @@
-var x = 'x';
+//각 함수는 전역 위에 올려져 있으므로 함수끼리 서로 접근하지 못한다.
+var x = "x";
 
 function findName() {
-  console.log(x);
-  var b = 'b';
+  console.log(c);
+  var b = "b";
   return printName();
 }
 
 function printName() {
-  var c = 'c';
-  return 'name';
+  console.log(x);
+  var c = "c";
+  return "name";
 }
 
 function sayName() {
-  var a = 'a';
-  return findName()
+  var a = "a";
+  return findName();
 }
 
 sayName();
 
+//중첩형태로 작성된 함수는 scope chain이 서로 물리게 된다.
 //lexically
 function say() {
-  var a = 'a';
+  var a = "a";
   return function find() {
-    var b = 'b';
+    var b = "b";
     return function print() {
-      var c = 'c';
+      var c = "c";
       console.log(a, b, c);
-      return 'name'
-    }
-  }
+      return "name";
+    };
+  };
 }
 
 say()()();
