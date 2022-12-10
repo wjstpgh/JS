b3767877663b509cf1a2ed1de16aadc721076cfe
wjstpgh <sehoJ3756@gmail.com>
2022-12-10 23:21:39 +0900

    즉시 실행함수 IIFE

--- IIFE.js
+++ IIFE.js
@@ -1,8 +1,9 @@
-//IIFE
-
+//IIFE 즉시 실행 함수
 (function () {
   var a = 1;
 })();
+//IIFE에서 정의된 변수 a는 외부에서 접근 불가
+console.log(a); //err
 
 // function(){}()//err
 
@@ -11,8 +12,8 @@ var script1 = (function () {
     return 5;
   }
   return {
-    a: a,
-  }
-})()
+    a: a(),
+  };
+})();
 
-
+script1;//IIFE함수는 저장되지 않고 실행 결과만 저장
