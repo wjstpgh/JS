cec5874b1cf6d39f59910a21cecc2fc379d2da72
wjstpgh <sehoJ3756@gmail.com>
2022-12-11 16:35:09 +0900

    함수 스코프와 블록 스코프

--- function&block.js
+++ function&block.js
@@ -3,23 +3,23 @@ if (5 > 4) {
   var secret = '123';
 }
 
-secret;
+secret;//'123'
 
 function a() {
   var secret = '123';
 }
 
-secret;
+secret;//err
 
 //block scope
 if (5 > 4) {
   let secret = '123';
 }
 
-secret;
+secret;//err
 
 function loop() {
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {//var or let
     console.log(i);
   }
   console.log('final', i);
