892507b9f79f973a94025448f3a3d6b448132515
wjstpgh <sehoJ3756@gmail.com>
2022-12-26 00:11:31 +0900

    호이스팅은 클로져 현상과 상관없음

---
+++ js프로토타입계층/HoistingOfClosour.js
@@ -0,0 +1,10 @@
+//Clusour Exercise
+function callMaybe() {
+  const callMe = "Hi! now here!";
+  setTimeout(function () {
+    console.log(callMe);
+  }, 4000);
+  // const callMe = "Hi! now here!";
+}
+
+callMaybe();
