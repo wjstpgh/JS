183c5c5432fc70556a5131f7b4cb527e94e6f9b3
wjstpgh <sehoJ3756@gmail.com>
2023-02-22 23:38:32 +0900

    명령형과 선언형 차이

---
+++ FP/imperative&declarative.js
@@ -0,0 +1,9 @@
+//Imperative vs Declarative
+//물잔을 달라는 요구에 기계와 사람에게 명령하는 체계가 다른 이유
+//imperative
+for (let i = 1; i < 4; i++) {
+  console.log(i);
+}
+
+//declarative
+[1, 2, 3].forEach((item) => console.log(item));
